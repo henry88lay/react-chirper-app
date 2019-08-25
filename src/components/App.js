e96f31491e5864972f38762b38f72b31cd905148
Henry Lay <henry88lay@gmail.com>
2019-08-25 20:53:21 +1000

    debugging the dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
 import {handleInitialData} from '../actions/shared';
 import {connect} from 'react-redux';
+import Dashboard from './Dashboard';
 
 class App extends Component {
   componentDidMount() {
     this.props.handleInitialData();
   }
   render() {
-    return <div>Starter Code</div>;
+    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
   }
 }
 
+function mapStateToProps({authedUser}) {
+  return {
+    loading: authedUser === null
+  };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   {handleInitialData}
 )(App);
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-dom';
 
 class Dashboard extends Component {
   render() {
     const {tweetsIds} = this.props;
-    return;
-    <div>
-      <h3 classname='center'>Your Timeline</h3>
-      <ul className='dashboard-list'>
-        {tweetsIds.map(id => (
-          <li key={id}>
-            <div>TWEET ID: {id}</div>
-          </li>
-        ))}
-      </ul>
-    </div>;
+    return (
+      <div>
+        <h3 classname='center'>Your Timeline</h3>
+        <ul className='dashboard-list'>
+          {tweetsIds.map(id => (
+            <li key={id}>
+              <div>TWEET ID: {id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
