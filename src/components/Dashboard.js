98d1ae2726251ceead2e31204fdd5a9e43841813
Henry Lay <henry88lay@gmail.com>
2019-08-25 20:11:45 +1000

    coded the dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {connect} from 'react-dom';
+
+class Dashboard extends Component {
+  render() {
+    const {tweetsIds} = this.props;
+    return;
+    <div>
+      <h3 classname='center'>Your Timeline</h3>
+      <ul className='dashboard-list'>
+        {tweetsIds.map(id => (
+          <li key={id}>
+            <div>TWEET ID: {id}</div>
+          </li>
+        ))}
+      </ul>
+    </div>;
+  }
+}
+
+function mapStateToProps({tweets}) {
+  return {
+    tweetsIds: Object.keys(tweets).sort(
+      (a, b) => tweets[b].timestamp - tweets[a].timestamp
+    )
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
