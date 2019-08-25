a56113efe6a0c32315abaedad215e59e5d49b622
Henry Lay <henry88lay@gmail.com>
2019-08-25 18:46:23 +1000

    added redux middleware code to entry point

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,8 @@
 import {thunk} from 'redux-thunk';
 import logger from './logger';
 import {applyMiddleWare} from 'redux';
+import middleware from './middleware';
+
+const store = createStore(reducer, middleware);
 
 export default applyMiddleWare(thunk, logger);
